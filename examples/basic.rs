879ca0b9500827252eb889f4b4544ce84bc40321
horup <soh@skov.dk>
2025-11-10 15:20:56 +0100

    wip

--- examples/basic.rs
+++ examples/basic.rs
@@ -1,4 +1,4 @@
-use ggsdk::GGRunOptions;
+use ggsdk::{GGAtlas, GGRunOptions};
 use glam::{Vec2, Vec3, Vec4};
 use glox::Glox;
 
@@ -12,6 +12,8 @@ impl ggsdk::GGApp for App {
         self.glox.init(g.gl);
         self.glox.camera.eye = Vec3::new(0.0, -10.0, 0.5);
         self.glox.camera.target = Vec3::default();
+
+        g.assets.load::<GGAtlas>("examples/wall_1x1.png", "wall");
     }
 
     fn update(&mut self, _: ggsdk::UpdateContext) {
@@ -26,9 +28,12 @@ impl ggsdk::GGApp for App {
     }
 
     fn paint_glow(&mut self, g: ggsdk::PaintGlowContext) {
+        let Some(texture) = g.assets.get::<GGAtlas>("wall") else { return };
+        let texture = g.painter.texture(texture.texture_id()).unwrap();
         let camera_dir= self.glox.camera.direction();
         let gl = g.painter.gl();
         let mut draw = self.glox.draw_builder(gl);
+        draw.bind_texture(Some(texture));
         draw.push_vertices(&glox::billboard_vertices(Default::default(), Vec4::splat(1.0), camera_dir, Vec2::splat(1.0)));
         draw.build();
     }
